b7b9493ef4d867dfda17bae8adabea3335e1a369
orenji_neko <markanthony.enfermo@outlook.com>
2026-01-17 11:36:25 +0800

    feat[enfermo]: add Landing Footer to root layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -6,6 +6,7 @@ import LandingHeader from "@/components/layout/header/LandingHeader";
 import LandingCountryNav from "@/components/layout/nav/LandingCountryNav";
 import LandingCategoryNav from "@/components/layout/nav/LandingCategoryNav";
 import { Categories, Countries } from "./data";
+import LandingFooter from "@/components/layout/footer/LandingFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,11 +36,12 @@ export default function RootLayout({
         <LandingHeader />
         <LandingCountryNav countries={Countries} />
         <LandingCategoryNav categories={Categories} />
-        <main className="flex-1 w-full">
+        <main className="flex-1 w-full bg-gray-100">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-12">
             {children}
           </div>
         </main>
+        <LandingFooter />
       </body>
     </html>
   );
