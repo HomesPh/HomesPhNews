33aa7e58c0cbee495b2fb1f12cfee30db5673ac3
Mark Enfermo <112786278+orenji-neko@users.noreply.github.com>
2026-01-20 15:44:18 +0800

    feat(client): add news store and restructure api types (#17)
    LGTM ~PM JM

--- client/lib/api/auth/types.ts
+++ client/lib/api/auth/types.ts
@@ -1,31 +1,3 @@
-export interface Author {
-  name: string;
-  imageUrl: string;
-}
-
-export interface RelatedArticle {
-  id: string;
-  title: string;
-  category: string;
-  image: string;
-  href: string;
-}
-
-export interface Article {
-  id: string | number;
-  country: string;
-  category: string;
-  title: string;
-  subtitle: string;
-  author: Author;
-  featuredImageUrl: string;
-  createdAt: string; // ISO Date string
-  views: number;
-  content: string;
-  topics: string[];
-  relatedArticles: RelatedArticle[];
-}
-
 export interface User {
   id: number;
   email: string;
