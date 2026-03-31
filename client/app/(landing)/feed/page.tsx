e6c07f3edfe8f2587806c5671cf46b26376aa8b9
GelaPostrero <143687976+GelaPostrero@users.noreply.github.com>
2026-03-31 10:02:11 +0800

    feat: re-route main landing to /feed and redirect root to login (#444)
    Looks good to me! ~PM JM!

---
+++ client/app/(landing)/feed/page.tsx
@@ -0,0 +1,40 @@
+import { Suspense } from "react";
+export const dynamic = 'force-dynamic';
+
+import DashboardFeed from "@/components/features/dashboard/DashboardFeed";
+import { getArticlesFeed, getRestaurants } from "@/lib/api-v2";
+import { DashboardSkeleton } from "@/components/features/dashboard/DashboardSkeletons";
+
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default async function Dashboard({ searchParams }: Props) {
+  const { country: countryParam, category: categoryParam } = await searchParams;
+
+  const country =
+    (Array.isArray(countryParam) ? countryParam[0] : countryParam) || "Global";
+  const category =
+    (Array.isArray(categoryParam) ? categoryParam[0] : categoryParam) || "All";
+
+  const response = getArticlesFeed({
+    country: country !== "Global" ? country : undefined,
+    category: (category !== "All" && category !== "Articles") ? category : undefined,
+  });
+
+  const restaurants = getRestaurants({
+    country: country !== "Global" ? country : undefined,
+    limit: 10,
+  });
+
+  return (
+    <Suspense fallback={<DashboardSkeleton />}>
+      <DashboardFeed
+        country={country}
+        category={category}
+        feed={response}
+        restaurants={restaurants}
+      />
+    </Suspense>
+  );
+}
